5dc45a2daa511c66eae3a984402038274918fec1
projetodesistemasweb <projetodesistemasweb@gmail.com>
2018-04-28 13:21:38 -0300

    projeto inicial

---
+++ src/main/java/com/projetoweb2/ProjetowebApplication.java
@@ -0,0 +1,12 @@
+package com.projetoweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetowebApplication.class, args);
+	}
+}
